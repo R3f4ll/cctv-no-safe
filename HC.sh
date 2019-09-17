98e18fa171fe30b2aa888364d3827b2fbe0200d1
R3f4ll <53784408+R3f4ll@users.noreply.github.com>
2019-09-18 04:58:02 +0700

    Add files via upload

---
+++ HC.sh
@@ -0,0 +1,230 @@
+#!/bin/bash
+#version 1.0
+
+clear
+sleep 2
+
+echo "sebelum masuk program"
+sleep 1
+echo "program ini tidak ada warna"
+sleep 1
+echo "klean akan masuk program dalam 3 detik"
+sleep 1
+figlet 1 | lolcat
+sleep 1
+figlet 2 | lolcat
+sleep 1
+figlet 3 | lolcat
+sleep 1
+clear
+
+figlet Hack CCTV | lolcat
+echo   "<=======================[]========================>"
+echo   "Tools    : Hack CCTV" |lolcat
+echo   "Creadby  : R3F4ll    " |lolcat
+echo   "Team     : Up_Cyber_Army  " |lolcat
+echo   "<=======================[]========================>"
+
+
+###################################################
+# CTRL + C
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo "[#]> (Ctrl + C ) Detected, Trying To Exit ... "
+echo "[#]> Thanks"
+sleep 1
+echo ""
+echo "[#]> see you gaes :)..."
+sleep 1
+exit
+}
+
+lagi=1
+while [ $lagi -lt 6 ];
+do
+echo ""
+echo  "1.  italia{endc}";
+echo  "============================" | lolcat
+echo  "2.  Indonesia{endc}";
+echo  "============================" | lolcat
+echo  "3.  japan{endc}";
+echo  "============================" | lolcat
+echo  "4   United states{endc}";
+echo  "============================" | lolcat
+echo  "5.  France{endc}";
+echo  "============================" | lolcat
+echo  "6. Korea{endc}";
+echo  "============================" | lolcat
+echo  "7. German{endc}";
+echo  "============================" | lolcat
+echo  "8. Turkey{endc}";
+echo  "============================" | lolcat
+echo  "00. Exit the program${endc}";
+echo ""
+echo -e "╭─R3F4LL" |lolcat
+read -p "╰─#" pil;
+
+# italia
+
+case $pil in
+1)echo " [http://195.223.42.87:83 ]"
+echo "[http://188.214.1.88:86]"
+echo "[http://151.58.36.82:10000]"
+echo "[ http://31.27.9.86:90 ]"
+echo "[ http://2.229.217.87:60001 ]"
+echo "[ http://151.61.186.86:60001 ]"
+echo "[ http://185.208.138.86:8080 ]"
+echo "[ http://77.108.9.82:8082 ]"
+echo "[ http://77.108.9.82:8081 ]"
+echo "[ http://2.44.7.85:80 ]"
+echo "[http://87.0.184.81:8080 ]"
+echo "[ http://79.21.88.83:8080 ]"
+echo "[ http://77.108.9.83:8081 ]"
+echo "[ http://80.180.135.80:82 ]"
+echo "[ http://151.32.1.80:82 ]"
+
+
+echo "makasih ya ^_^"
+exit
+
+;;
+
+# indonesia
+
+2)echo  "[http://119.252.173.43:82 ]"
+echo "[ http://119.252.173.43:81 ]"
+echo "[ http://119.252.173.43:83 ]"
+echo "[ http://119.252.173.43:84 ]"
+echo "[ http://36.66.133.249:80 ]"
+echo "[ http://101.128.67.165:60001 ]"
+echo "[ http://115.124.85.163:83 ]"
+echo "[ http://222.165.247.20:80 ]"
+echo "[ http://222.165.247.20:8080 ]"
+echo "[ http://222.165.208.18:50001 ]"
+
+
+echo "makasih ya ^_^"
+exit
+
+;;
+
+#Japan
+
+3)echo  "[http://118.12.38.82:8001 ]"
+echo "[ http://111.233.209.87:80 ]"
+echo "[ http://153.129.103.87:82 ]"
+echo "[ http://118.22.53.84:50000 ]"
+echo "[ http://118.22.53.84:50001 ]"
+echo "[http://180.47.249.86:8000 ]"
+echo "[ http://163.44.60.84:80 ]"
+echo "[ http://119.170.209.81:9000 ]"
+echo "[ http://115.65.218.83:80 ]"
+echo "[ http://118.14.202.83:60001 ]"
+echo "[http://61.206.127.83:80 ]"
+echo "[ http://210.153.211.79:8081 ]"
+echo "[ http://210.153.211.79:8080 ]"
+echo "[ http://153.166.196.79:82 ]"
+echo "[ http://220.107.201.79:80 ]"
+
+
+echo "makasih ya ^_^"
+exit
+
+;;
+
+#United states
+
+4) echo "[ http://73.25.132.87:8080 ]"
+echo "[ http://166.168.200.86:81 ]"
+echo "[ http://73.248.32.84:80 ]"
+echo "[ http://68.175.10.82:60001 ]"
+echo "[ http://173.91.145.87:60001 ]"
+echo "[ http://75.133.149.87:60001 ]"
+echo "[ http://208.64.73.84:80 ]"
+echo "[ http://172.87.8.82:50001 ]"
+echo "[ http://172.87.8.82:50000 ]"
+echo "[ http://24.147.68.84:8081 ]"
+
+
+echo "makasih ya ^_^"
+exit
+
+;;
+
+#France
+
+5) echo "[ http://90.46.135.87:8080 ]"
+echo "[ http://81.250.184.83:80 ]"
+echo "[ http://81.249.221.80:80 ]"
+echo "[ http://78.245.72.80:8084 ]"
+echo "[ http://217.128.4.81:8083 ]"
+echo "[ http://5.48.45.80:60001 ]"
+echo "[ http://82.64.66.72:80 ]"
+echo "[ http://78.196.240.75:8001 ]"
+echo "[ http://185.50.78.71:2000 ]"
+echo "[ http://78.240.104.76:80 ]"
+
+
+echo "makasih ya ^_^"
+exit
+
+;;
+
+#Korea
+
+6) echo "[ http://58.121.75.84:8000 ]"
+echo "[ http://121.66.137.83:8000 ]"
+echo "[ http://203.142.184.80:5000 ]"
+echo "[ http://221.141.122.79:8000 ]"
+echo "[ http://117.123.161.79:8000 ]"
+echo "[ http://221.139.136.75:5000 ]"
+echo "[ http://112.155.121.75:8000 ]"
+echo "[ http://39.122.158.76:8000 ]"
+echo "[ http://61.105.212.75:8000 ]"
+echo "[ http://221.139.94.72:8000 ]"
+
+
+echo "makasih ya ^_^"
+exit
+;;
+
+#German
+
+7) echo "[ http://93.196.24.86:8000 ]"
+echo "[ http://137.193.65.86:8080 ]"
+echo "[ http://178.27.178.87:80 ]"
+echo "[ http://87.171.218.87:9000 ]"
+echo "[ http://91.103.119.88:10000 ]"
+
+
+echo "makasih ya ^_^"
+exit
+;;
+
+#turkey
+
+8) echo "[ http://212.50.60.157:113 ]"
+echo "[ http://212.50.60.157:100 ]"
+echo "[ http://212.50.51.61:163 ]"
+echo "[ http://212.50.60.153:163 ]"
+echo "[ http://212.50.60.56:199 ]"
+
+
+echo "maaksih ya ^_^"
+exit
+;;
+
+
+
+00) echo "created by : [UCA]•Mr.R3F4LL✓" | lolcat
+exit
+;;
+*) echo "[*]Sorry,Pilihan lu gk ada, gw clear dulu"
+sleep 2
+clear
+esac
+done
+done
+
